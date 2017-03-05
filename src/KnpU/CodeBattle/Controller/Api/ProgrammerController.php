2db36b3afa717dccebbc789590876b9c5abd0662
sungvadan <svd.phan@gmail.com>
2017-03-05 21:39:36 +0100

    POST: Creation, Location Header

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -8,6 +8,7 @@ use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use KnpU\CodeBattle\Model\Programmer;
 
 class ProgrammerController extends BaseController
 {
@@ -16,10 +17,23 @@ class ProgrammerController extends BaseController
         $controllers->post('/api/programmers', array($this, 'newAction'));
     }
 
-    public function newAction(){
-        return 'let\'s Battle!';
-    }
+    public function newAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
 
+        $programmer = new Programmer();
+        $programmer->nickname = $data['nickname'];
+        $programmer->avatarNumber = $data['avatarNumber'];
+        $programmer->tagLine = $data['tagLine'];
+        $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+
+        $this->save($programmer);
+
+        $response = new Response('It worked! Trust me I\'m an API',201);
+        $response->headers->set('Location', '/programmers/not/fake....');
+
+        return $response;
+    }
 
 
 }
